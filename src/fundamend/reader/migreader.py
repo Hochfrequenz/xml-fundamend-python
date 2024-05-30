b289075535854f60b97dc8173e9ea52c44b9c2f6
konstantin <konstantin.klein@hochfrequenz.de>
2024-05-30 18:23:53 +0200

    move element distinction logic to separate module (#7)

--- src/fundamend/reader/migreader.py
+++ src/fundamend/reader/migreader.py
@@ -15,41 +15,13 @@ from fundamend.models.messageimplementationguide import (
     Segment,
     SegmentGroup,
 )
-
-
-def _is_segment_group(element: ET.Element) -> bool:
-    """
-    returns True if the given element is a segment group
-    """
-    return element.tag.startswith("G_SG")
-
-
-def _is_segment(element: ET.Element) -> bool:
-    """
-    returns True if the given element is a segment
-    """
-    return element.tag.startswith("S_")
-
-
-def _is_data_element_group(element: ET.Element) -> bool:
-    """
-    returns True if the given element is a data element group
-    """
-    return element.tag.startswith("C_")
-
-
-def _is_data_element(element: ET.Element) -> bool:
-    """
-    returns True if the given element is a data element
-    """
-    return element.tag.startswith("D_")
-
-
-def _is_code(element: ET.Element) -> bool:
-    """
-    returns True if the given element is a code
-    """
-    return element.tag == "Code"
+from fundamend.reader.element_distinction import (
+    _is_code,
+    _is_data_element,
+    _is_data_element_group,
+    _is_segment,
+    _is_segment_group,
+)
 
 
 def _to_code(element: ET.Element) -> Code:
