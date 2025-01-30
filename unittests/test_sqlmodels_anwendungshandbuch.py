ffc956da466c4bc27ed19206ecff24cb14517d7e
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-01-30 16:41:48 +0100

    wip

--- unittests/test_sqlmodels_anwendungshandbuch.py
+++ unittests/test_sqlmodels_anwendungshandbuch.py
@@ -2,16 +2,15 @@
 we try to fill a database using kohlrahbi[sqlmodels] and the data from the machine-readable AHB submodule
 """
 
-import json
 from pathlib import Path
 from typing import Generator
 
 import pytest
-from sqlmodel import Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine
 
 from fundamend import AhbReader
 from fundamend.models.anwendungshandbuch import Anwendungshandbuch as PydanticAnwendunghandbuch
-from fundamend.sqlmodels.anwendungshandbuch import Anwendungshandbuch
+from fundamend.sqlmodels.anwendungshandbuch import Anwendungshandbuch as SqlAnwendungshandbuch
 
 
 @pytest.fixture()
@@ -27,7 +26,7 @@ def sqlite_session(tmp_path: Path) -> Generator[Session, None, None]:
 def _load_anwendungshandbuch_ahb_to_and_from_db(
     session: Session, pydantic_ahb: PydanticAnwendunghandbuch
 ) -> PydanticAnwendunghandbuch:
-    sql_ahb = Anwendungshandbuch.from_model(pydantic_ahb)
+    sql_ahb = SqlAnwendungshandbuch.from_model(pydantic_ahb)
     session.add(sql_ahb)
     session.commit()
     session.refresh(sql_ahb)
@@ -35,9 +34,20 @@ def _load_anwendungshandbuch_ahb_to_and_from_db(
     return pydantic_ahb_after_roundtrip
 
 
-def test_sqlmodels_anwendungshandbuch(sqlite_session: Session, tmp_path: Path) -> None:
+def test_sqlmodels_single_anwendungshandbuch(sqlite_session: Session) -> None:
     ahb = AhbReader(
         Path(__file__).parent / "example_files" / "UTILTS_AHB_1.1d_Konsultationsfassung_2024_04_02.xml"
     ).read()
     roundtrip_abb = _load_anwendungshandbuch_ahb_to_and_from_db(sqlite_session, ahb)
+    ahb_json = ahb.model_dump(mode="json")
+    roundtrip_json = roundtrip_abb.model_dump(mode="json")
+    assert ahb_json == roundtrip_json  # in pycharm the error message is much better when comparing plain python dicts
     assert roundtrip_abb == ahb
+
+def test_sqlmodels_all_anwendungshandbuch(sqlite_session: Session) -> None:
+    private_submodule_root = Path(__file__).parent.parent/"xml-migs-and-ahbs"
+    assert private_submodule_root.exists() and private_submodule_root.is_dir()
+    for ahb_file_path in private_submodule_root.rglob("**/*AHB*.xml"):
+        ahb = AhbReader(ahb_file_path).read()
+        roundtrip_abb = _load_anwendungshandbuch_ahb_to_and_from_db(sqlite_session, ahb)
+        assert roundtrip_abb == ahb
