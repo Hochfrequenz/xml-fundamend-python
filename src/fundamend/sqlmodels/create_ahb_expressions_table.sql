7e1d59457aeb115e7792514714b228edd3a2278c
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-04-15 11:54:54 +0200

    wip

--- src/fundamend/sqlmodels/create_ahb_expressions_table.sql
+++ src/fundamend/sqlmodels/create_ahb_expressions_table.sql
@@ -30,7 +30,6 @@ FROM (
            format,
            pruefidentifikator,
            dataelement_ahb_status AS expression,
-           anwendungsfall_pk,
            anwendungshandbuch_primary_key
     FROM ahb_hierarchy_materialized
     UNION ALL
@@ -38,14 +37,13 @@ FROM (
            format,
            pruefidentifikator,
            code_ahb_status AS expression,
-           anwendungsfall_pk,
            anwendungshandbuch_primary_key
     FROM ahb_hierarchy_materialized
 )
-WHERE expression is not null AND TRIM(expression) = ''
+WHERE expression is not null AND TRIM(expression) != ''
 ORDER BY edifact_format_version, format, pruefidentifikator, expression;
 
 CREATE UNIQUE INDEX idx_ahb_expressions_id ON ahb_expressions (id);
-CREATE INDEX idx_ahb_expressions_expression ON ahb_expressions (expression);
-CREATE INDEX idx_ahb_expressions_metadata ON ahb_expressions (edifact_format_version, format, pruefidentifikator);
-CREATE UNIQUE INDEX idx_ahb_expressions_expression ON ahb_expressions (edifact_format_version, format, pruefidentifikator, expression);
+CREATE INDEX  idx_ahb_expressions_expression ON ahb_expressions (expression);
+CREATE INDEX  idx_ahb_expressions_metadata ON ahb_expressions (edifact_format_version, format, pruefidentifikator);
+CREATE UNIQUE INDEX idx_ahb_expressions_metadata_expression ON ahb_expressions (edifact_format_version, format, pruefidentifikator, expression);
---
+++ unittests/test_sqlmodels_expressions_view.py
@@ -0,0 +1,38 @@
+
+
+from datetime import date
+from pathlib import Path
+
+import pytest
+from sqlmodel import Session, create_engine, select
+from syrupy.assertion import SnapshotAssertion
+
+from fundamend.sqlmodels import create_db_and_populate_with_ahb_view
+from fundamend.sqlmodels.expression_view import create_and_fill_ahb_expression_table, AhbExpression
+
+
+@pytest.mark.snapshot
+def test_create_db_and_expressions_view(snapshot: SnapshotAssertion) -> None:
+    ahb_paths = [
+        (
+            Path(__file__).parent / "example_files" / "UTILTS_AHB_1.1d_Konsultationsfassung_2024_04_02.xml",
+            date(2023, 10, 1),
+            date(2024, 4, 3),
+        )
+    ]
+    actual_sqlite_path = create_db_and_populate_with_ahb_view(ahb_files=ahb_paths, drop_raw_tables=True)
+    assert actual_sqlite_path.exists()
+    engine = create_engine(f"sqlite:///{actual_sqlite_path}")
+    with Session(bind=engine) as session:
+        create_and_fill_ahb_expression_table(session)
+        stmt = (
+            select(AhbExpression)
+            .where(AhbExpression.pruefidentifikator == "25001")
+        )
+        results = session.exec(stmt).all()
+    raw_results = [r.model_dump(mode="json") for r in results]
+    for raw_result in raw_results:
+        for guid_column in ["id"]:  # there's no point to compare those
+            if guid_column in raw_result:
+                del raw_result[guid_column]
+    snapshot.assert_match(raw_results)
