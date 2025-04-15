7a32e608558bd209712488014869faea244a99b2
kevin <kevin.krechan@hochfrequenz.de>
2025-04-15 08:58:54 +0200

    update cli.py to new structure

--- src/fundamend/cli.py
+++ src/fundamend/cli.py
@@ -1,22 +1,20 @@
 """contains the entrypoint for the command line interface"""
 
-import json
-from pathlib import Path
-
 import typer
-from pydantic import RootModel
 from rich.console import Console
-from typing_extensions import Annotated
 
-from fundamend import AhbReader, Anwendungshandbuch, MessageImplementationGuide, MigReader
+from fundamend.commands import app as commands_app
 
-app = typer.Typer(name="xml2json", help="Convert XML(s) by BDEW to JSON(s)")
+app = typer.Typer(name="fundamend", help="CLI tool to work with XML files by BDEW", no_args_is_help=True)
 err_console = Console(stderr=True)  # https://typer.tiangolo.com/tutorial/printing/#printing-to-standard-error
+
+
+app.add_typer(commands_app)
 
 
 def cli() -> None:
     """entry point of the script defined in pyproject.toml"""
-    typer.run(main)
+    typer.run(app)
 
 
 if __name__ == "__main__":
