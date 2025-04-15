cc2097f346fab58690c412dec14596f40a8783d4
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-04-15 15:02:40 +0200

    wip

--- src/fundamend/sqlmodels/create_ahb_expressions_table.sql
+++ src/fundamend/sqlmodels/create_ahb_expressions_table.sql
@@ -1,8 +1,7 @@
 -- This SQLite script selects all ahb_status from various other tables into one common table.
 -- There is a Pydantic model class for the 'ahb_expressions' table: AhbExpression
 
-DROP TABLE IF EXISTS ahb_expressions;
-CREATE TABLE ahb_expressions AS
+INSERT INTO ahb_expressions (id, edifact_format_version, format, pruefidentifikator, expression, node_texts, anwendungshandbuch_primary_key)
 SELECT DISTINCT
     hex(randomblob(16)) AS id,
     edifact_format_version,
@@ -10,7 +9,7 @@ SELECT DISTINCT
     pruefidentifikator,
     expression,
     '' AS node_texts, -- will be filled with the help of ahbicht in python
-   anwendungshandbuch_primary_key
+    anwendungshandbuch_primary_key
 FROM (
     SELECT edifact_format_version,
            format,
@@ -40,10 +39,10 @@ FROM (
            anwendungshandbuch_primary_key
     FROM ahb_hierarchy_materialized
 )
-WHERE expression is not null AND TRIM(expression) != ''
+WHERE expression IS NOT NULL AND TRIM(expression) != ''
 ORDER BY edifact_format_version, format, pruefidentifikator, expression;
 
-CREATE UNIQUE INDEX idx_ahb_expressions_id ON ahb_expressions (id);
-CREATE INDEX  idx_ahb_expressions_expression ON ahb_expressions (expression);
-CREATE INDEX  idx_ahb_expressions_metadata ON ahb_expressions (edifact_format_version, format, pruefidentifikator);
+-- Create indexes
+CREATE INDEX idx_ahb_expressions_expression ON ahb_expressions (expression);
+CREATE INDEX idx_ahb_expressions_metadata ON ahb_expressions (edifact_format_version, format, pruefidentifikator);
 CREATE UNIQUE INDEX idx_ahb_expressions_metadata_expression ON ahb_expressions (edifact_format_version, format, pruefidentifikator, expression);
