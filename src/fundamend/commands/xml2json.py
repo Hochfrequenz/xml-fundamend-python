e3140b6d369a3365f75a9df2bbb370624b422995
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2025-06-30 12:54:31 +0200

    Add option to output AHB as directory of JSON files for each "Prüfi"

--- src/fundamend/commands/xml2json.py
+++ src/fundamend/commands/xml2json.py
@@ -17,14 +17,44 @@ from fundamend.sanitize import sanitize_ahb
 FORMAT_AND_TYPE_REGEX = re.compile(r"^([A-Z]+)_(AHB|MIG)_(?:(Gas|Strom)_)?")
 
 
+def _write_ahb_models_splitted(
+    model: Anwendungshandbuch,
+    ahb_dir: Path,
+    *,
+    compressed: bool = False,
+) -> None:
+    """Writes the given Anwendungshandbuch model to multiple JSON files, one for each Anwendungsfall."""
+    ahb_dir.mkdir(parents=True, exist_ok=True)
+    for anwendungsfall in model.anwendungsfaelle:
+        json_file_path = ahb_dir / f"{anwendungsfall.pruefidentifikator}.json"
+        with open(json_file_path, encoding="utf-8", mode="w") as outfile:
+            outfile.write(anwendungsfall.model_dump_json(indent=None if compressed else 2))
+
+    # Write meta file
+    ahb_meta_file_path = ahb_dir / "meta.json"
+    with open(ahb_meta_file_path, encoding="utf-8", mode="w") as outfile:
+        outfile.write(model.model_dump_json(exclude={"anwendungsfaelle"}, indent=None if compressed else 2))
+
+
 def _write_model_to_json_file(
-    model: Anwendungshandbuch | MessageImplementationGuide, xml_file_path: Path, *, compressed: bool = False
+    model: Anwendungshandbuch | MessageImplementationGuide,
+    xml_file_path: Path,
+    *,
+    compressed: bool = False,
+    split_ahb: bool = False,
 ) -> None:
     """Writes the given model to a JSON file at the specified path."""
-    json_file_path = xml_file_path.with_suffix(".json")
-    with open(json_file_path, encoding="utf-8", mode="w") as outfile:
-        outfile.write(model.model_dump_json(indent=None if compressed else 1))
-    typer.echo(f"Successfully converted {xml_file_path} to JSON {json_file_path}")
+    if split_ahb:
+        if not isinstance(model, Anwendungshandbuch):
+            raise ValueError("split_ahb can only be used with Anwendungshandbuch models")
+        ahb_dir = xml_file_path.with_suffix("")
+        _write_ahb_models_splitted(model, ahb_dir, compressed=compressed)
+        typer.echo(f"Successfully converted {xml_file_path} to multiple JSON files in {ahb_dir}")
+    else:
+        json_file_path = xml_file_path.with_suffix(".json")
+        with open(json_file_path, encoding="utf-8", mode="w") as outfile:
+            outfile.write(model.model_dump_json(indent=None if compressed else 2))
+        typer.echo(f"Successfully converted {xml_file_path} to JSON {json_file_path}")
 
 
 def _convert_to_json_files(
@@ -46,7 +76,9 @@ def _convert_to_json_files(
     return mig_model, ahb_model
 
 
-def xml2json_dir_mode(xml_path: Path, sanitize: bool = False, compressed: bool = False) -> None:
+def xml2json_dir_mode(
+    xml_path: Path, sanitize: bool = False, compressed: bool = False, split_ahb: bool = False
+) -> None:
     """
     Converts all XML files in the given directory to JSON files.
     The function expects to find pairs of MIG and AHB XML files in the directory.
@@ -81,10 +113,12 @@ def xml2json_dir_mode(xml_path: Path, sanitize: bool = False, compressed: bool =
         mig_path = _xmls_and_matches_list[1][0]
         mig, ahb = _convert_to_json_files(mig_path, ahb_path, sanitize=sanitize)
         _write_model_to_json_file(mig, mig_path.with_suffix(".json"), compressed=compressed)
-        _write_model_to_json_file(ahb, ahb_path.with_suffix(".json"), compressed=compressed)
+        _write_model_to_json_file(ahb, ahb_path.with_suffix(".json"), compressed=compressed, split_ahb=split_ahb)
 
 
-def xml2json_file_mode(xml_path: Path, sanitize: bool = False, compressed: bool = False) -> None:
+def xml2json_file_mode(
+    xml_path: Path, sanitize: bool = False, compressed: bool = False, split_ahb: bool = False
+) -> None:
     """
     Converts a single XML file to JSON.
     The function expects to find the corresponding AHB or MIG file in the same directory.
@@ -116,7 +150,7 @@ def xml2json_file_mode(xml_path: Path, sanitize: bool = False, compressed: bool 
         _write_model_to_json_file(mig, xml_path.with_suffix(".json"), compressed=compressed)
     else:
         mig, ahb = _convert_to_json_files(other_matches[0], xml_path, sanitize=sanitize)
-        _write_model_to_json_file(ahb, xml_path.with_suffix(".json"), compressed=compressed)
+        _write_model_to_json_file(ahb, xml_path.with_suffix(".json"), compressed=compressed, split_ahb=split_ahb)
 
 
 @app.command()
@@ -159,6 +193,18 @@ def xml2json(
             " (default), the output JSON files will be pretty-printed with an indentation of one space.",
         ),
     ] = False,
+    split_ahb: Annotated[
+        bool,
+        typer.Option(
+            ...,
+            "--split-ahb",
+            "-a",
+            help="If set, the AHB will be split into multiple files, one for each Anwendungsfall. "
+            "The files will be named `<Prüfidentifikator>.json` in a directory named after the AHB file's "
+            "name (without the extension). It will contain an additional `meta.json` file containing the fields of "
+            "`Anwendungshandbuch` except for `anwendungsfaelle`.",
+        ),
+    ] = False,
 ) -> None:
     """
     Converts the xml file(s) from `xml_in_path` to a json file next to the `*.xml`.
@@ -167,6 +213,6 @@ def xml2json(
     All xml files must follow the naming convention `/^(?P<FORMAT>[A-Z]+)_(AHB|MIG)_((Gas|Strom)_)?.*\\.xml$/`
     """
     if xml_path.is_dir():
-        xml2json_dir_mode(xml_path, sanitize=sanitize, compressed=compressed)
+        xml2json_dir_mode(xml_path, sanitize=sanitize, compressed=compressed, split_ahb=split_ahb)
     else:
-        xml2json_file_mode(xml_path, sanitize=sanitize, compressed=compressed)
+        xml2json_file_mode(xml_path, sanitize=sanitize, compressed=compressed, split_ahb=split_ahb)
